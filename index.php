95a2a41cf21c11a42ff128c0617878deda8b022a
eriksebastian88 <30632841+eriksebastian88@users.noreply.github.com>
2022-01-09 07:39:25 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <meta charset="utf8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aplikasi Berbasis Website</title>
+    <link rel="stylesheet" href="app.css">
+  </head>
+  <body>
+    <div class="hero">
+      <div class="hero-text">
+        <h1>Aplikasi Berbasis Website</h1>
+        <p>
+          Aplikasi ini didesign dengan menggunakan teknologi HTML, CSS, Bootstrap, JS, Ajax, JQuery, PHP dan MySQL
+        </p>
+        <button>Daftar / Login</button>
+      </div>
+    </div>
+  </body>  
+</html>
